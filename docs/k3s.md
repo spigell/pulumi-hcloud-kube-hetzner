2c4b5b2551f44dc1eb5c63710f41ae4eb046a353
Spigell <spigelly@gmail.com>
2023-12-08 17:21:19 +0400

    fix test

--- docs/k3s.md
+++ docs/k3s.md
@@ -4,6 +4,8 @@ The program allows to manage k3s versions both of methods:
 - exact version specified in the configuration;
 - using [system-upgrade-controller](https://github.com/rancher/system-upgrade-controller) and crd `Plan` (version and channel are available);
 
+The manual aproach allows for downgrade and upgrade k3s cluster to the specified version, but it is not recommended to use it in production. The automatic aproach is more suitable for production, but it is not possible to downgrade k3s cluster using it.
+
 Also the k3s well-known label `k3s-upgrade` will be added if system-upgrade-controller is enabled. But the user can disable it setting `k3s-upgrade=false` in k3s label section.
 
 The following table describes error combinations:
--- internal/integration/integration.go
+++ internal/integration/integration.go
@@ -20,11 +20,12 @@ const (
 	envConfigSource = "PULUMI_CONFIG_SOURCE"
 	envConfigPath   = "PULUMI_STACK_CONFIG"
 
-	exampleK3SPrivateNonHASimple  = "k3s-private-non-ha-simple"
-	exampleK3SWGNonHaFwRules      = "k3s-wireguard-non-ha-firewall-rules"
-	exampleK3SWGHANoTaints        = "k3s-wireguard-ha-no-taints"
-	exampleK3SPublicNonHADefaults = "k3s-public-non-ha-with-defaults"
-	exampleK3SPublicHAKubeAddons  = "k3s-public-ha-kube-addons"
+	exampleK3SPrivateNonHASimple   = "k3s-private-non-ha-simple"
+	exampleK3SWGNonHaFwRules       = "k3s-wireguard-non-ha-firewall-rules"
+	exampleK3SWGHANoTaints         = "k3s-wireguard-ha-no-taints"
+	exampleK3SPublicNonHADefaults  = "k3s-public-non-ha-with-defaults"
+	exampleK3SPublicHAKubeAddons   = "k3s-public-ha-kube-addons"
+	exampleK3SPrivateNonHAUpgrader = "k3s-private-non-ha-upgrader"
 
 	testWGConnectivity                    = "wireguard-connectivity"
 	testKubeVersion                       = "kube-version"
@@ -32,7 +33,8 @@ const (
 	testKubeChangeEndpointType            = "kube-change-endpoint-type"
 	testKubeHetznerCCM                    = "kube-hetzner-ccm"
 	testNodeChangeLabelsTaints            = "node-change-labels-taints"
-	testKubeK3SUpgradeControllerPlanValid = "kube-k3s-upgrade-controller-plan-valid"
+	testKubeK3SUpgradeControllerPlan      = "kube-k3s-upgrade-controller-plan"
+	testKubeK3SUpgradeControllerConfigEnv = "kube-k3s-upgrade-controller-config-env"
 )
 
 // TestsByExampleName is a map of tests and their test cases.
@@ -41,7 +43,7 @@ var TestsByExampleName = map[string][]string{
 	exampleK3SPrivateNonHASimple: {
 		testSSHConnectivity,
 		testKubeHetznerCCM,
-		testKubeK3SUpgradeControllerPlanValid,
+		testKubeK3SUpgradeControllerPlan,
 	},
 	exampleK3SWGNonHaFwRules: {
 		testSSHConnectivity,
@@ -53,7 +55,7 @@ var TestsByExampleName = map[string][]string{
 		testWGConnectivity,
 		testKubeVersion,
 		testNodeChangeLabelsTaints,
-		testKubeK3SUpgradeControllerPlanValid,
+		testKubeK3SUpgradeControllerPlan,
 	},
 	exampleK3SPublicNonHADefaults: {
 		testSSHConnectivity,
@@ -63,7 +65,11 @@ var TestsByExampleName = map[string][]string{
 		testSSHConnectivity,
 		testKubeHetznerCCM,
 		testNodeChangeLabelsTaints,
-		testKubeK3SUpgradeControllerPlanValid,
+		testKubeK3SUpgradeControllerPlan,
+	},
+	exampleK3SPrivateNonHAUpgrader: {
+		testKubeK3SUpgradeControllerPlan,
+		testKubeK3SUpgradeControllerConfigEnv,
 	},
 }
 
